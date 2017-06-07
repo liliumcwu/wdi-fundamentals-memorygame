af578463670646a648886b6b72093af51f937f70
Lilium Wu <lilium.wu@yale.edu>
2017-06-07 14:58:03 -0700

    finishes unit 9 assignment

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,23 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1])
-    alert("You found a match!");
-  else alert("Sorry, try again.");
-
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+  }
 }
 
+var flipCard = function(cardId) {
+  console.log("You just flipped " + cards[cardId])
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
 
 
