37b924f495b10ffb1f02e85a01d909fa853d456f
Lilium Wu <lilium.wu@yale.edu>
2017-06-08 12:27:25 -0700

    finishes unit 11 assignment with reset button bonus

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,7 @@ var cards = [
   {
     rank: "queen",
     suit: "hearts",
-    cardImage: "image/queen-of-hearts.png"
+    cardImage: "images/queen-of-hearts.png"
   },
   {
     rank: "queen",
@@ -23,7 +23,17 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-var checkForMatch = function() {
+var flipCard = function(evt) {
+  console.log('this', this);
+  //var cardId = this.getAttribute('data-id');
+  var cardId = this.getAttribute('data-id');
+  // console.log("You just flipped " + cards[cardId].rank)
+  // console.log("Image is " + cards[cardId].cardImage);
+  // console.log("Suit is " + cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
+
+  this.setAttribute('src', cards[cardId].cardImage);
+  // checkForMatch();
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match!");
@@ -33,15 +43,29 @@ var checkForMatch = function() {
   }
 }
 
-var flipCard = function(cardId) {
-  console.log("You just flipped " + cards[cardId].rank)
-  console.log("Image is " + cards[cardId].cardImage);
-  console.log("Suit is " + cards[cardId].suit);
-  cardsInPlay.push(cards[cardId].rank);
-  checkForMatch();
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    document.getElementById('game-board').appendChild(cardElement);
+    cardElement.addEventListener('click', flipCard);
+
+
+    document.getElementById('reset-button').addEventListener('click', resetCards);
+  }
 }
 
-flipCard(0);
-flipCard(2);
+var resetCards = function() {
+  console.log('this is ' + this);
+  var cardElements = document.getElementById('game-board').childNodes;
+  console.log("cardElements is " + cardElements);
+  for (var i = 0; i < cardElements.length; i++) {
+    cardElements[i].setAttribute('src', 'images/back.png');
+  }
+  cardsInPlay = [];
+}
+
+createBoard();
 
 
